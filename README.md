567583ce81da5b80d3854ad6bb4b29fc87e6a8de
Seth Nelson <seth.nelson@outlook.com>
2020-05-17 21:00:32 -0700

    finished all CRUD operations except PUT for actions, but got all other requests to work.

--- README.md
+++ README.md
@@ -39,14 +39,19 @@ Commit your code regularly and use descriptive messages. This helps both you (in
 Demonstrate your understanding of this Sprint's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your Team Lead.
 
 - [ ] Mention two parts of Express that you learned about this week.
+    `Express allows you to build restful web services and perform operations on client requests to an API.`
 
 - [ ] Describe Middleware?
+    `Middleware allows you to insert certain functions into the server code in a particular order.`
 
 - [ ] Describe a Resource?
+    `A resource is `
 
 - [ ] What can the API return to help clients know if a request was successful?
+    `A json message object with a proper error code is a good UI practice.`
 
 - [ ] How can we partition our application into sub-applications?
+    `You can `
 
 ## Minimum Viable Product
 
@@ -56,7 +61,7 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 Design and build the necessary endpoints to:
 
 - [x] Perform CRUD operations on _projects_ and _actions_. When adding an action, make sure the `project_id` provided belongs to an existing `project`. If you try to add an action with an `id` of 3 and there is no project with that `id` the database will return an error.
-- [ ] Retrieve the list of actions for a project.
+- [x] Retrieve the list of actions for a project.
 
 Please read the following sections before implementing the Minimum Viable Product, they describe how the database is structured and the files and methods available for interacting with the data.
 
--- router/actionRoute.js
+++ router/actionRoute.js
@@ -6,24 +6,56 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
+    actionData
+        .get()
+        .then(projects => res.status(200).json(projects))
+        .catch(() => res.status(500).json({ message: 'The actions could not be retrieved from the database.'}));
+});
 
-})
-
+// test works? yes
 router.get('/:id', (req, res) => {
-    
+    const { id } = req.params
+    actionData.get(id)
+        .then(action =>  {
+            if(action.id > 0) res.status(200).json(action)
+            else res.status(400).json({ message: 'The project id could not be retrieved from the database.'}) 
+        .catch(() => res.status(404).json({ message: 'Actions could not be retrieved.'}))
+        })
 })
 
+// almost works
 router.post('/', (req, res) => {
-    
-})
+    const newAction = req.body;
+    if (newAction.description === '') {
+        res.status(400).json({ message: 'Please provide a description of the action.'})
+} else {
+    actionData.insert(newAction)
+        .then(action => res.status(200).json(action))
+        .catch(() => res.status(500).json({ message: 'Could not post the action to the database.'}));
+    }
+});
 
+// test works? yes
 router.put('/:id', (req, res) => {
-    
+    const { id } = req.params;
+    const changes = req.body;
+    if (changes.description) {
+        actionData.update(id, changes)
+            .then(updatedAction => res.status(200).json(updatedAction))
+            .catch(() => res.status(500).json({ message: 'The action information could not be modified.'}))
+    } else res.status(400).json({message: 'Please modify name or description to update'});
 })
 
+// test works? yes
 router.delete('/:id', (req, res) => {
-    
-})
+    const { id } = req.params
+    actionData.remove(id)
+    .then(deleted => {
+        if(deleted) res.status(200).json({ message: 'The action has been deleted.'})
+        else res.status(404).json({ message: 'The action with this id does not exist'})
+    })
+    .catch(() => res.status(500).json({ message: 'The action could not be deleted.'}))
+});
 
 
 module.exports = router;
